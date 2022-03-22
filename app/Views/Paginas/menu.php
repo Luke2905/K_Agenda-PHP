01cf883e0d349d48f8ea5213236b3975dbcc9449
Luke2905 <laranjeiralucas17@gmail.com>
2022-03-21 21:37:03 -0300

    Alteração Layout

---
+++ app/Views/Paginas/menu.php
@@ -0,0 +1,15 @@
+<body>
+
+    <header>
+        <div class="center">
+            <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+            <div class="logo"><a href="index.html">Kings.Agendamento</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a class="selected" href="">Agendamentos</a></li>
+                    <li><a href="">Galeria</a></li>
+                    <li><a href="">Profissional</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
