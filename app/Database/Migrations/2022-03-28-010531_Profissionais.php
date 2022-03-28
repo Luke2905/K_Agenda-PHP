40c1210b4b8399847e9f940708e9d7c73fe7df84
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-03-27 22:12:29 -0300

    Crição Model/Migration Profissionais

---
+++ app/Database/Migrations/2022-03-28-010531_Profissionais.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Profissionais extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            [
+                'id_pro' =>[
+                    'type' => 'INT',
+                    'constraint' =>10,
+                    'unsigned'       => true,
+                 'auto_increment' => true,
+                ],
+                'nome_pro' =>[
+                    'type' => 'VARCHAR',
+                    'constraint' =>100,
+                    'null' => false,
+                ],
+                'telefone_pro' =>[
+                    'type' => 'INT',
+                    'constraint' =>11,
+                    'null' => false,
+                ],
+                'foto_pro' =>[
+                    'type' => 'VARCHAR',
+                    'constraint' => 2083,
+                    'null' => false,
+                ],
+            ]
+            );
+            $this->forge->addKey('id_pro', true);
+            $this->forge->createTable('profissionais');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('profissionais');
+    }
+}
