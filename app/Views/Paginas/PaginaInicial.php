37c5b94eb6deaea829f6eef13b6662ddd0a911f5
Emanuel <emanuelssobral@gmail.com>
2022-03-31 19:54:35 -0300

    Changes and add styles

--- app/Views/Paginas/PaginaInicial.php
+++ app/Views/Paginas/PaginaInicial.php
@@ -1,4 +1,4 @@
- <div class="container_parallax">
+<div class="container_parallax">
      <main>
          <section class="parallax">
             <h1>Kings</h1>
@@ -38,3 +38,61 @@
         </div>
     </div>
  </section>
+
+ <div class="comentarios">
+  <div class="wrapper">
+    <div class="outer">
+      <div class="card" style="--delay:-1;">
+        <div class="content">
+          <div class="img"><img src="https://img.r7.com/images/ariel-barbeiro-instagram-sucesso-18122018131137018" alt=""></div>
+          <div class="details">
+            <span class="name">Sumit Kapoor</span>
+            <p>Profissionais e experientes</p>
+          </div>
+        </div>
+        <a href="#">Gostei</a>
+      </div>
+      <div class="card" style="--delay:0;">
+        <div class="content">
+          <div class="img"><img src="https://img.r7.com/images/ariel-barbeiro-instagram-sucesso-18122018131137018" alt=""></div>
+          <div class="details">
+            <span class="name">Andrew Neil</span>
+            <p>Preço excelente</p>
+          </div>
+        </div>
+        <a href="#">Gostei</a>
+      </div>
+      <div class="card" style="--delay:1;">
+        <div class="content">
+          <div class="img"><img src="https://img.r7.com/images/ariel-barbeiro-instagram-sucesso-18122018131137018" alt=""></div>
+          <div class="details">
+            <span class="name">Jasmine Carter</span>
+            <p>Otimo lugar e otimo preço</p>
+          </div>
+        </div>
+        <a href="#">Gostei</a>
+      </div>
+      <div class="card" style="--delay:2;">
+        <div class="content">
+          <div class="img"><img src="https://img.r7.com/images/ariel-barbeiro-instagram-sucesso-18122018131137018" alt=""></div>
+          <div class="details">
+            <span class="name">Justin Chung</span>
+            <p>Excelentes profissionais</p>
+          </div>
+        </div>
+        <a href="#">Gostei</a>
+      </div>
+      <div class="card" style="--delay:2;">
+        <div class="content">
+          <div class="img"><img src="https://img.r7.com/images/ariel-barbeiro-instagram-sucesso-18122018131137018" alt=""></div>
+          <div class="details">
+            <span class="name">Adrina Calvo</span>
+            <p>Otimos Cortes</p>
+          </div>
+        </div>
+        <a href="#">Gostei</a>
+      </div>
+    </div>
+  </div>
+
+</div>
