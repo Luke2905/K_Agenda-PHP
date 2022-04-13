9e47882c27211dd31cf3277babcdeca4cfea623f
Emanuel <emanuelssobral@gmail.com>
2022-04-13 18:57:58 -0300

    Logo, Imgs, font and localization changes

--- app/Views/layout_padrao.php
+++ app/Views/layout_padrao.php
@@ -5,6 +5,9 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="/estilo.css" />
   <link rel="stylesheet" href="/flickity.css">
   <script src="/flickity.pkgd.js"></script>
@@ -13,7 +16,7 @@
 
 <body>
 <header id="header">
-    <a id="logo" href="/Home/index">Kings Agendamento</a>
+    <a id="logo" href="/Home/index"><img src="/img/logo_branco.png" alt=""></a>
     <nav id="nav">
       <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
         <span id="hamburger"></span>
@@ -39,16 +42,20 @@
 
 <footer class="footer1">
   <div class="center">
-    <div class="w501">
+    <div class="w50">
       <p><i class="fas fa-envelope"></i> kingcut@gmail.com</p>
       <p><i class="fas fa-phone-alt"></i> (XX) 9XXXX-XXXX</p>
     </div>
   </div>
-  <div class="w502">
+  <div class="w50">
     <a href="https://twitter.com/n_oels"><i class="fab fa-twitter"></i> /n_oels</a>
     <br />
     <a href="https://www.instagram.com/n.oel.s/"><i class="fab fa-instagram"></i> /n.oel.s</a>
-    <br />>
+    <br />
+    <a href="https://twitter.com/n_oels"><i class="fab fa-twitter"></i> /n_oels</a>
+    <br />
+    <a href="https://www.instagram.com/n.oel.s/"><i class="fab fa-instagram"></i> /n.oel.s</a>
+    <br />
   </div>
   <div class="clear"></div>
 </footer>
--- app/Views/localizacao.php
+++ app/Views/localizacao.php
@@ -3,14 +3,36 @@
 <?php $this->section("localizacao");?>
 
 <div class="barbearia_img_localizacao">
-    <a href="/Home/mapa">
+  <a href="/Home/mapa">
     <main>
-    <section class="barbearia_img_localizacao_parallax">
-      <h1>Kings</h1>
-    </section>
-  </main>
-    </a>
-
+      <section class="barbearia_img_localizacao_parallax">
+        <style >
+        </style>
+        <h1>Kings - Diadema</h1>
+      </section>
+    </main>
+  </a>
 </div>
+
+<div class="barbearia_img_localizacao">
+  <a href="/Home/mapa">
+    <main>
+      <section class="barbearia_img_localizacao_parallax">
+        <h1>Kings - São Bernado</h1>
+      </section>
+    </main>
+  </a>
+</div>
+
+<div class="barbearia_img_localizacao">
+  <a href="/Home/mapa">
+    <main>
+      <section class="barbearia_img_localizacao_parallax">
+        <h1>Kings - Santo Andre</h1>
+      </section>
+    </main>
+  </a>
+</div>
+
 
 <?php $this->endSection(); ?>
