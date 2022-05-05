0e48cfef9e00fef2babc25ba4b4d55068d52328f
Luke2905 <laranjeiralucas17@gmail.com>
2022-05-05 20:30:40 -0300

    Função para cadastro de usuarios

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -10,4 +10,29 @@ class Admin extends BaseController
     {
         echo view('admin/home');
     }
+
+    public function novoUser()
+    {
+        echo view('admin/novo_user');
+    }
+
+    public function cadUser()
+    {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+
+        $model = model('LoginModel');
+        $dados = $this->request->getPost();
+        $user = $model ->insert($dados);
+
+        if($user){
+            echo "<span class='help-block' style='color: Blue;'>Cadastro efetuado com sucesso!</span>";
+            echo "<a href='/Home/login'>Pagina de Login</a>";
+    }else {
+        echo "<span class='help-block' style='color: Red;'>Erro no cadastro!</span>";
+    }
+
+    }
 }
+
