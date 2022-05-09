06e691b2b2fbaba8a0a9f036802fd5f5e6a8fb3c
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-08 22:58:31 -0300

    exibição de funcionarios cadastrados no banco de dados

--- app/Views/funcionarios.php
+++ app/Views/funcionarios.php
@@ -3,32 +3,26 @@
 <?php $this->section("funcionarios");?>
 
 <div class="container_func">
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://2.bp.blogspot.com/-RgZLWAA9GyA/XNcfooj5qjI/AAAAAAAAGxU/2Vh6ugZGad8Xu_Px4ZUP2E8LJ7cGVj9FwCLcBGAs/s1600/corte-cabelo-curto-2.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <a href="">Agendar</a>
-    </div>
+    
+<?php 
+    $model = model('LoginModel');
+    $dados = $model ->find();
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://defrenteparaomar.com/wp-content/10-beleza/202010-cabelo-medio-liso/10-corte-de-cabelo-medio-liso.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <a href="">Agendar</a>
-    </div>
+foreach($dados as $row){
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-a.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <a href="">Agendar</a>
-    </div>
+    echo "<div class='card'>";
+    echo "<div class='CardImagem'>";
+    echo "<img  src='https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-a.jpg' alt=''>";
+    echo "</div>";
+    echo "<h3>".$row['nome']."</h3>";
+    echo "<h4>".$row['email']."</h4>";
+    echo "<a href=''>Agendar</a>";
+    echo "</div>";
+
+
+}
+
+?>
 </div>
 
 <?php $this->endSection(); ?>
