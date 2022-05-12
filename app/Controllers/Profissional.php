e3047543f499f22b8d6081b3ea0c1f05cd5a754e
Luke2905 <laranjeiralucas17@gmail.com>
2022-05-12 20:17:14 -0300

    Metodo de cadastro de Profissionais

--- app/Controllers/Profissional.php
+++ app/Controllers/Profissional.php
@@ -16,4 +16,24 @@ class Profissional extends BaseController
        // dd($dados);
         return view('funcionarios',$dados);
     }
+
+    public function cadProfissional(){
+
+        $nome = $_POST['nome'];
+        $url = $_POST['url'];
+        $telefone = $_POST['telefone'];
+
+        $model = model('ProfissionalModel');
+        $dados = $this->request->getPost();
+        $pro = $model ->db->query("insert into profissionais(nome_pro,telefone_pro,foto_pro) values ('$nome','$telefone','$url')");
+
+        if($pro){
+            echo "<span class='help-block' style='color: Blue;'>Cadastro efetuado com sucesso!</span><br>";
+            echo "<a href='/Home/login'>Pagina de Login</a><br>";
+            echo "<a href='/Profissional/index'>Pagina de Profissionais</a>";
+    }else {
+        echo "<span class='help-block' style='color: Red;'>Erro no cadastro!</span>";
+    }
+
+    }
 }
