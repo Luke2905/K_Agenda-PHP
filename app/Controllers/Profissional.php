1f36f0632ccb6ea7186991e5a7f040d38b40a928
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-15 18:55:23 -0300

    Metodo para deletar profissional

--- app/Controllers/Profissional.php
+++ app/Controllers/Profissional.php
@@ -13,19 +13,19 @@ class Profissional extends BaseController
     }
     public function index()
     {$dados['profissionais'] = $this->profissionalmodel->findAll();
-       // dd($dados);
-        return view('funcionarios',$dados);
+        if($dados>0){
+            return view('funcionarios',$dados);
+        }else{
+            echo "nenhum profissional cadastrado";
+            echo "<a href='/Home/index'>Pagina Inicial</a><br>";
+        }
     }
 
     public function cadProfissional(){
 
-        $nome = $_POST['nome'];
-        $url = $_POST['url'];
-        $telefone = $_POST['telefone'];
-
         $model = model('ProfissionalModel');
         $dados = $this->request->getPost();
-        $pro = $model ->db->query("insert into profissionais(nome_pro,telefone_pro,foto_pro) values ('$nome','$telefone','$url')");
+        $pro = $model ->db->query("insert into profissionais(nome_pro,telefone_pro,foto_pro) values ('$dados[nome]','$dados[telefone]','$dados[url]')");
 
         if($pro){
             echo "<span class='help-block' style='color: Blue;'>Cadastro efetuado com sucesso!</span><br>";
@@ -34,6 +34,27 @@ class Profissional extends BaseController
     }else {
         echo "<span class='help-block' style='color: Red;'>Erro no cadastro!</span>";
     }
+
+    }
+
+    public function delProfissional(){
+
+        $model = model('ProfissionalModel');
+        $dados = $this->request->getPost();
+        $pro = $model ->db->query("delete from profissionais where id_pro = '$dados[id]'");
+
+        if($pro){
+            echo "<span class='help-block' style='color: Blue;'>deletado com sucesso!</span><br>";
+            echo "<a href='/Home/login'>Pagina de Login</a><br>";
+            echo "<a href='/Profissional/index'>Pagina de Profissionais</a>";
+    }else {
+        echo "<span class='help-block' style='color: Red;'>Não foi Possivel deletar o Usuario!</span>";
+    }
+
+    }
+    public function delPro(){
+        $dados['profissionais'] = $this->profissionalmodel->findAll();
+        return view('/admin/del_pro',$dados);
 
     }
 }
