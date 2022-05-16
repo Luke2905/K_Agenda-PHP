a5ec4acf1ddecec99c4974b1e841fcf150c5d0dd
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-15 23:28:32 -0300

    Menus de gerenciamento

---
+++ app/Views/admin/menu_pro.php
@@ -0,0 +1,20 @@
+<?php $this->extend("admin/layout_adm");?>
+
+<?php $this->section("Menu_Pro");?>
+<h1 class="titulo">Tela de Administração dos Profissionias</h1>
+<div class="menu_adm">
+    <div id="item1" class="btn-group">
+    <a href="/Profissional/cadPro" class="btn  btn-dark active" aria-current="page">Cadastrar</a>
+    </div>
+    <div id="item2" class="btn-group">
+    <a href="/Profissional/delPro" class="btn btn-warning active" aria-current="page">Deletar</a>
+    </div>
+    <div id="item3" class="btn-group">
+    <a href="#" class="btn btn-dark active" aria-current="page">Alterar</a>
+    </div>
+    <div id="item4" class="btn-group">
+    <a href="/Profissional/index" class="btn btn-warning active" aria-current="page">Ver</a>
+    </div>
+</div>
+
+<?php $this->endSection(); ?>
---
+++ app/Views/admin/menu_serv.php
@@ -0,0 +1,20 @@
+<?php $this->extend("admin/layout_adm");?>
+
+<?php $this->section("Menu_Serv");?>
+<h1 class="titulo">Tela de Administração dos Cortes e Serviços</h1>
+<div class="menu_adm">
+    <div id="item1" class="btn-group">
+    <a href="/Servico/cadServ" class="btn  btn-warning active" aria-current="page">Cadastrar</a>
+    </div>
+    <div id="item2" class="btn-group">
+    <a href="/Servico/delServ" class="btn btn-dark active" aria-current="page">Deletar</a>
+    </div>
+    <div id="item3" class="btn-group">
+    <a href="#" class="btn btn-warning active" aria-current="page">Alterar</a>
+    </div>
+    <div id="item4" class="btn-group">
+    <a href="/Servico/index" class="btn btn-dark active" aria-current="page">Ver</a>
+    </div>
+</div>
+
+<?php $this->endSection(); ?>
