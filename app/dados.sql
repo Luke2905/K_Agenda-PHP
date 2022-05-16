e4a7693308c1219c4ea2ff8fd2262c769ff91b2a
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-15 23:56:02 -0300

    insert

---
+++ app/dados.sql
@@ -0,0 +1,26 @@
+-- dados para a tabela Serviços --
+
+insert into servicos(nome_serv,preco_serv,foto_serv) values
+('Low fade',45,'https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-ae.jpg'),
+('High fade',40,'https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-b.jpg'),
+('Lateral com navalha',60,'https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-h.jpg'),
+('Degradê',35,'https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-o.jpg'),
+('Flat Top',50,'https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-z-.jpg'),
+('Fade Drop',56,'https://blog.trinks.com/wp-content/uploads/2019/08/img_8910-993x1024.jpg'),
+('Black Power',60,'https://blog.trinks.com/wp-content/uploads/2019/08/3bf8f376d089a72e027fecf72acd2dfe.jpg'),
+('Nudred',30,'https://blog.trinks.com/wp-content/uploads/2019/08/flat_top-768x1024.jpg'),
+(' Risco de Navalha',20,'https://blog.trinks.com/wp-content/uploads/2019/08/corte-degrad%C3%AA-masculino-cabelo-crespo.jpg');
+
+-- dados para a tabela Profissionais --
+
+insert into profissionais(nome_pro,telefone_pro,foto_pro) values
+('Alberto',999703020,'https://img.ibxk.com.br/2019/02/17/17124052466014.jpg'),
+('Vania',981521926,'https://cajamar.sp.gov.br/noticias/wp-content/uploads/sites/2/2021/07/site-vacinacao-33-anos.png'),
+('Larissa',997335130,'https://img.freepik.com/fotos-gratis/a-foto-horizontal-agradou-a-mulher-de-pele-escura-com-corte-de-cabelo-afro-aponta-para-longe-com-os-dois-dedos-da-frente-mostra-um-espaco-em-branco-para-sua-promocao-isolado-sobre-a-parede-azul-pessoas-conceito-de-propaganda_273609-26380.jpg?w=2000'),
+('Valter',986271580,'https://cajamar.sp.gov.br/noticias/wp-content/uploads/sites/2/2021/08/site-vacinacao-26-anos.png'),
+('Mike',972519113,'https://pm1.narvii.com/6583/13022a93a381cddb0c98d4e0a813635bd1215d89_hq.jpg'),
+('Nelson',999673212,'https://static1.conquistesuavida.com.br/articles//4/56/84/@/18404-gente-que-da-prioridade-aos-sentimentos-article_gallery-2.jpg'),
+('Ryan',983711677,'https://img.freepik.com/fotos-gratis/pessoa-reflexiva-calma-pacifica-pessoa-criativa-em-pe-com-a-mao-perto-dos-labios_259150-20819.jpg'),
+('Erick',991316875,'https://tangerino.com.br/blog/wp-content/uploads/2021/03/cursos-de-gestao-de-pessoas-1.jpg'),
+('Antonieta',997035342,'https://fotografiadicas.com.br/wp-content/uploads/2017/10/15-Como-fotografar-pessoas-Fotografia-Dicas.jpg');
+
