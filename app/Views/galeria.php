8a02afa786e7d50c0e487e561052770ba130af09
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-15 23:22:25 -0300

    exibindo cortes cadastrados no banco

--- app/Views/galeria.php
+++ app/Views/galeria.php
@@ -3,120 +3,51 @@
 <?php $this->section("galeria");?>
 
 <div class="container">
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://2.bp.blogspot.com/-RgZLWAA9GyA/XNcfooj5qjI/AAAAAAAAGxU/2Vh6ugZGad8Xu_Px4ZUP2E8LJ7cGVj9FwCLcBGAs/s1600/corte-cabelo-curto-2.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://defrenteparaomar.com/wp-content/10-beleza/202010-cabelo-medio-liso/10-corte-de-cabelo-medio-liso.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+<?php 
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-a.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+foreach($servicos as $row){
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://barbeariatatuape.com.br/wp-content/uploads/2020/02/corte-americano-9.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://2.bp.blogspot.com/-RgZLWAA9GyA/XNcfooj5qjI/AAAAAAAAGxU/2Vh6ugZGad8Xu_Px4ZUP2E8LJ7cGVj9FwCLcBGAs/s1600/corte-cabelo-curto-2.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+    echo " <div class='card'>";
+    echo "<div class='CardImagem'>";
+    echo "<img  src='".$row['foto_serv']."'alt=''>";
+    echo "</div>";
+    echo "<h3>".$row['nome_serv']."</h3>";
+    echo "<h4>R$ ".$row['preco_serv']."</h4>";
+    echo "<p>Corte chave de mais confia</p>";
+    echo "</div>";
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://defrenteparaomar.com/wp-content/10-beleza/202010-cabelo-medio-liso/10-corte-de-cabelo-medio-liso.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-a.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+}
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://barbeariatatuape.com.br/wp-content/uploads/2020/02/corte-americano-9.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://2.bp.blogspot.com/-RgZLWAA9GyA/XNcfooj5qjI/AAAAAAAAGxU/2Vh6ugZGad8Xu_Px4ZUP2E8LJ7cGVj9FwCLcBGAs/s1600/corte-cabelo-curto-2.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+?>
+</div>
+<?php $this->endSection(); ?>
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://defrenteparaomar.com/wp-content/10-beleza/202010-cabelo-medio-liso/10-corte-de-cabelo-medio-liso.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+<?php $this->extend("admin/layout_adm");?>
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://homensquesecuidam.com/wp-content/uploads/2017/10/cortes-de-cabelo-masculino-curto-homens-que-se-cuidam-a.jpg"
-                alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+<?php $this->section("Servicos");?>
 
-    <div class="card">
-        <div class="CardImagem">
-            <img src="https://barbeariatatuape.com.br/wp-content/uploads/2020/02/corte-americano-9.jpg" alt="">
-        </div>
-        <h3>CORTE</h3>
-        <h4>VALOR</h4>
-        <p>Corte chave de mais confia</p>
-    </div>
+<div class="container">
+    
+<?php 
+
+foreach($servicos as $row){
+
+    echo " <div class='card'>";
+    echo "<div class='CardImagem'>";
+    echo "<img  src='".$row['foto_serv']."'alt=''>";
+    echo "</div>";
+    echo "<h3>".$row['nome_serv']."</h3>";
+    echo "<h4>R$ ".$row['preco_serv']."</h4>";
+    echo "<p>Corte chave de mais confia</p>";
+    echo "</div>";
+
+
+}
+
+?>
 </div>
 
+<a style="color:black;" href="/Admin/menuServ">Voltar</a>
 <?php $this->endSection(); ?>
