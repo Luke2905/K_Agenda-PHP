4f21fb61a71965f08aec4f26f3f103bccd456e0e
Hisllaylla <hisllayllak@gmail.com>
2022-05-24 18:26:50 -0300

    [feat] Implementação inicial do Bootstrap no projeto

--- app/Views/layout_padrao.php
+++ app/Views/layout_padrao.php
@@ -1,80 +1,231 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
-  <link id="estilo" class="dia" rel="stylesheet" href="/estilo.css" />
-  <link rel="stylesheet" href="/flickity.css">
-  <script src="/flickity.pkgd.js"></script>
-  <title>King.Agendamentos</title>
-</head>
-
-<!--Header-->
-<body>
-
-<label for="modo-escuro">
-    <a class="button">Modo dark</a>
-</label>
-<input type="checkbox" id="modo-escuro">
-<script src="/estilo.js"></script>
-
-<header id="header">
-    <a id="logo" href="/Home/index"><img src="/img/logo_branco.png" alt=""></a>
-    <nav id="nav">
-      <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
-        <span id="hamburger"></span>
-      </button>
-      <ul id="menu" role="menu">
-        <li><a href="/Servico/index">Galeria</a></li>
-        <li><a href="/Profissional/index">Profissional</a></li>
-        <li><a href="/Home/agendamento">Agendamento</a></li>
-        <li><a href="/Home/localizacao">Localizacão</a></li>
-        <li><a href="/Home/login">Login</a></li>
-      </ul>
-    </nav>
-  </header>
-  <script src="/menu_mobile.js"></script>
-
-  <?php echo $this->renderSection("pagina_inicial");?>
-  <?php echo $this->renderSection("galeria");?>
-  <?php echo $this->renderSection("funcionarios");?>
-  <?php echo $this->renderSection("agendamento");?>
-  <?php echo $this->renderSection("localizacao");?>
-  <?php echo $this->renderSection("register_login");?>
-  <?php echo $this->renderSection("login_register");?>
-  <?php echo $this->renderSection("mapa");?>
-
-</body>
-
-<!--footer-->
-
-<footer class="footer1">
-  <div class="center">
-    <div class="w50">
-      <p class="footer_p"><i class="fas fa-envelope"></i> kingcut@gmail.com</p>
-      <p class="footer_p">(XX) 9XXXX-XXXX</p>
-      <br>
-    </div>
-  </div>
-  <div class="w50">
-    <a href="https://twitter.com/n_oels"><i class="fab fa-twitter"></i> /n_oels</a>
-    <br />
-    <a href="https://www.instagram.com/n.oel.s/"><i class="fab fa-instagram"></i> /n.oel.s</a>
-    <br />
-    <a href="https://twitter.com/n_oels"><i class="fab fa-twitter"></i> /n_oels</a>
-    <br />
-    <a href="https://www.instagram.com/n.oel.s/"><i class="fab fa-instagram"></i> /n.oel.s</a>
-    <br />
-  </div>
-  <div class="clear"></div>
-</footer>
-<footer class="footer2">
-  <p class="footer_p">Atividade de PRWII - ETECJK - Professor: Jefferson</p>
-</footer>
-
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+      <!-- Custom Carousel -->
+      <link rel="stylesheet" href="/carousel.css" />
+      <style>
+         .bd-placeholder-img {
+         font-size: 1.125rem;
+         text-anchor: middle;
+         -webkit-user-select: none;
+         -moz-user-select: none;
+         -ms-user-select: none;
+         user-select: none;
+         }
+         @media (min-width: 768px) {
+         .bd-placeholder-img-lg {
+         font-size: 3.5rem;
+         }
+         }
+      </style>
+      <title>King Agendamentos</title>
+   </head>
+   <body>
+      <header>
+         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#">
+            <img src="/img/logo_branco.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            King Agendamentos
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Galeria</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Profissional</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled">Agendamento</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled">Localizacão</a>
+                  </li>
+               </ul>
+            </div>
+            <form class="form-inline mt-2 mt-md-0">
+               <button class="btn btn-secondary my-2 my-sm-0" type="submit">Login</button>
+            </form>
+            </div>
+         </nav>
+      </header>
+      <main role="main">
+         <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+               <li data-target="#myCarousel" data-slide-to="1"></li>
+               <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title> </title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em"> </text>
+                  </svg> -->
+                  <img src="/img/barber1.png" alt="">
+                  <div class="container">
+                     <div class="carousel-caption text-left">
+                        <h1>Example headline.</h1>
+                        <p>Some representative placeholder content for the first slide of the carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title> </title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em"> </text>
+                  </svg> -->
+                  <img src="/img/barber2.png" alt="">
+                  <div class="container">
+                     <div class="carousel-caption">
+                        <h1>Another example headline.</h1>
+                        <p>Some representative placeholder content for the second slide of the carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" role="img" aria-label=" :  " preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title> </title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em"> </text>
+                  </svg> -->
+                  <img src="/img/barber3.png" alt="">
+                  <div class="container">
+                     <div class="carousel-caption text-right">
+                        <h1>One more for good measure.</h1>
+                        <p>Some representative placeholder content for the third slide of this carousel.</p>
+                        <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-target="#myCarousel" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-target="#myCarousel" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </button>
+         </div>
+         <!-- Marketing messaging and featurettes
+            ================================================== -->
+         <!-- Wrap the rest of the page in another container to center all the content. -->
+         <div class="container marketing">
+            <!-- Three columns of text below the carousel -->
+            <div class="row">
+               <div class="col-lg-4">
+                  <!-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                  </svg> -->
+                  <img src="/img/emanuel.png" width="250" height="250" alt="">
+                  <h2>Heading</h2>
+                  <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+               </div>
+               <!-- /.col-lg-4 -->
+               <div class="col-lg-4">
+                  <!-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                  </svg> -->
+                  <img src="/img/hisllaylla.png" width="250" height="250" alt="">
+                  <h2>Heading</h2>
+                  <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+               </div>
+               <!-- /.col-lg-4 -->
+               <div class="col-lg-4">
+                  <!-- <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#777"/>
+                     <text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
+                  </svg> -->
+                  <img src="/img/lucas.png" width="250" height="250" alt="">
+                  <h2>Heading</h2>
+                  <p>And lastly this, the third column of representative placeholder content.</p>
+               </div>
+               <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+            <!-- START THE FEATURETTES -->
+            <hr class="featurette-divider">
+            <div class="row featurette">
+               <div class="col-md-7">
+                  <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+                  <p class="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+               </div>
+               <div class="col-md-5">
+                  <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#eee"/>
+                     <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                  </svg> -->
+                  <img src="/img/barber1.png" width="400" height="400" alt="">
+               </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+               <div class="col-md-7 order-md-2">
+                  <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+                  <p class="lead">Another featurette? Of course. More placeholder content here to give you an idea of how this layout would work with some actual real-world content in place.</p>
+               </div>
+               <div class="col-md-5 order-md-1">
+                  <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#eee"/>
+                     <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                  </svg> -->
+                  <img src="/img/barber2.png" width="400" height="400" alt="">
+               </div>
+            </div>
+            <hr class="featurette-divider">
+            <div class="row featurette">
+               <div class="col-md-7">
+                  <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+                  <p class="lead">And yes, this is the last block of representative placeholder content. Again, not really intended to be actually read, simply here to give you a better view of what this would look like with some actual content. Your content.</p>
+               </div>
+               <div class="col-md-5">
+                  <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                     <title>Placeholder</title>
+                     <rect width="100%" height="100%" fill="#eee"/>
+                     <text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text>
+                  </svg> -->
+                  <img src="/img/barber3.png" width="400" height="400" alt="">
+               </div>
+            </div>
+            <hr class="featurette-divider">
+            <!-- /END THE FEATURETTES -->
+         </div>
+         <!-- /.container -->
+         <!-- FOOTER -->
+         <footer class="container">
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2017-2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+         </footer>
+      </main>
+      <!-- Optional JavaScript; choose one of the two! -->
+      <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+      <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+      <!-- Option 2: Separate Popper and Bootstrap JS -->
+      <!--
+         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+         -->
+   </body>
 </html>
