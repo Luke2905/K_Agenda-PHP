a17edddf6b7fe83bb4114f1daa2f4eadb1c8c64c
Wagner nailson <83068741+SetZeroMD@users.noreply.github.com>
2022-05-23 21:52:37 -0300

    Add files via upload

--- app/Database/Migrations/2022-05-19-230816_Agendamento.php
+++ app/Database/Migrations/2022-05-19-230816_Agendamento.php
@@ -16,12 +16,24 @@ class Agendamento extends Migration
                 'unsigned'  => true,
              'auto_increment' => true,
             ],
-        ],
+            'nome_agend' =>[
+                'type' => 'VARCHAR',
+                'constraint' =>100,
+                'null' => false,
+            ],
+            'email_agend' =>[
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => false,
+            ],
+        ]
         );
+        $this->forge->addKey('id_agend', true);
+            $this->forge->createTable('agendamento');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('agendamento');
     }
 }
