616c18492a60debbb2efc98c194ffbb20d433a4b
nicolealternat <coloca seu email da etec aqui>
2022-05-25 20:28:41 -0300

    DarkBox changes and button style

--- public/estilo.js
+++ public/estilo.js
@@ -2,19 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const darkModeStorage = localStorage.getItem('dark-mode')
     const root = document.querySelector(':root')
     const inputDarkMode = document.getElementById('modo-escuro')
+    const checkbox_dark = document.getElementById("checkbox_dark")
 
     if(darkModeStorage){
         root.classList.add("dark-mode");
         inputDarkMode.checked = true;
+        checkbox_dark.classList.toggle('on')
     }
 
-    inputDarkMode.addEventListener('change', () => {
-        if(inputDarkMode.checked){
-            root.classList.add("dark-mode");
-            localStorage.setItem('dark-mode', true);
-        }else{
+    checkbox_dark.addEventListener('click', (event) => {
+        if (checkbox_dark.classList.contains('on')) {
+            checkbox_dark.setAttribute('aria-checked', 'false');
             root.classList.remove("dark-mode");
             localStorage.removeItem('dark-mode');
+        } else {
+            checkbox_dark.setAttribute('aria-checked', 'true');
+            root.classList.add("dark-mode");
+            localStorage.setItem('dark-mode', true);
         }
-   })
+        checkbox_dark.classList.toggle('on');
+    });
 })
