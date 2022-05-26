a48ba8d425242bbb011f57f165aa7b80610ecf4e
KaioSena <88010959+KaioSena@users.noreply.github.com>
2022-05-26 20:31:10 -0300

    Add files via upload
    foreign keys

--- app/Database/Migrations/2022-05-19-230816_Agendamento.php
+++ app/Database/Migrations/2022-05-19-230816_Agendamento.php
@@ -16,6 +16,11 @@ class Agendamento extends Migration
                 'unsigned'  => true,
              'auto_increment' => true,
             ],
+            'id_pro' =>[
+                'type' => 'INT',
+                'constraint' =>10,
+                'unsigned'       => true,
+            ],
             'nome_agend' =>[
                 'type' => 'VARCHAR',
                 'constraint' =>100,
@@ -29,11 +34,14 @@ class Agendamento extends Migration
         ]
         );
         $this->forge->addKey('id_agend', true);
-            $this->forge->createTable('agendamento');
+         $this->forge->createTable('agendamento');
+        $this->forge->foreignkey('id_pro');
+
     }
 
     public function down()
     {
         $this->forge->dropTable('agendamento');
+        $this->forge->dropForeignKey('agendamento','id_pro');
     }
 }
