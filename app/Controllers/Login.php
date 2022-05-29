b615a4bc58d5088e8989a3088f5bb2fd0dea8c2d
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-29 20:05:06 -0300

    Criação do Controller Login com a função autenticar

---
+++ app/Controllers/Login.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Login extends BaseController
+{
+    public function autenticar()
+    {
+        $model = model('LoginModel');
+        $dados = $this->request->getPost();
+        
+        $model->where('email',$dados['usuario']);
+        $resultado = $model->where('senha',$dados['senha'])->find();
+
+       if(count($resultado)>0){
+           return view('/admin/home');
+       }else{
+        $model = model('ProfissionalModel');
+        $dados = $this->request->getPost();
+        
+        $model->where('email_pro',$dados['usuario']);
+        $resultado = $model->where('senha_pro',$dados['senha'])->find();
+        if(count($resultado)>0){
+            echo "<a>Profissional Logado</a><br>";
+            echo "<a href='/Home/index'>Sair</a><br>";
+        }else{
+           echo "<span class='help-block' style='color: Red;'>Não foi Possivel fazer login!</span>";
+           header("Refresh:1; url=/Home/Login");
+        }
+       }
+    }
+}
