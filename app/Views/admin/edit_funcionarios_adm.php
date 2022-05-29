3ee0b027d93028b4ae0e9bc6379441f82af40cf9
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-05-29 20:07:03 -0300

    Recebimento de dados a serem alterados

--- app/Views/admin/edit_funcionarios_adm.php
+++ app/Views/admin/edit_funcionarios_adm.php
@@ -1,8 +1,8 @@
-<?php $this->extend("layout_padrao");?>
+<?php $this->extend("admin/layout_adm");?>
 
-<?php $this->section("funcionarios");?>
+<?php $this->section("Alterar_pro");?>
 
-<section class="pesquisa">
+<!-- <section class="pesquisa">
     <div class="center">
         <form action="/Profissional/pesquisarPro" method="post">
             <h2>Busque nossos funcionarios</h2>
@@ -10,18 +10,22 @@
             <input type="submit" value="pesquisar">
         </form>
     </div>
-</section>
-
-<div class="edit_container_func">
+</section> -->
+<h1 class="titulo" >Dados do Profissional</h1>
+<a style="margin:10px;" class="btn btn-warning" href="/Profissional/selectPro" role="button">Voltar</a>
+<form  action="/Profissional/altProfissional" method="post">
+    <div class="edit_container_func">
     <div class='card'>
+    <input value='<?php echo $profissionais['id_pro'];?>' type="number" placeholder="codigo" name="id" readonly>
         <div class='CardImagem'>
-            <img  src='https://cdn-icons-png.flaticon.com/512/32/32339.png' alt=''>
+            <img  src='<?php echo $profissionais['foto_pro'];?>' alt=''>
         </div>
-            <input placeholder="Foto Profissional">
-            <input placeholder="Nome Profissional">
-            <input placeholder="Numero Telefone">
-            <a href=''>Editar</a>
+            <input value='<?php echo $profissionais['foto_pro'];?>' type="text" name="url"placeholder="Foto Profissional">
+            <input value='<?php echo $profissionais['nome_pro'];?>' type="text" name="nome" placeholder="Nome Profissional">
+            <input value='<?php echo $profissionais['telefone_pro'];?>' type="phone" name="telefone"placeholder="Numero Telefone">
+            <button  type="submit" class="btn btn-primary" >Salvar</button>
     </div>
 </div>
+</form>
 
 <?php $this->endSection(); ?>
