e0c09bcdbcfed8e72ddf0cd4318a77f28938a0cb
Lucas de Barros <laranjeiralucas17@gmail.com>
2022-06-26 20:52:56 -0300

    titulo da página

--- app/Views/errors/erro_agendamento.php
+++ app/Views/errors/erro_agendamento.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <title>Erro</title>
+    <title>Aviso</title>
     <style>
         .container{
             width: 100%;
@@ -26,7 +26,7 @@
 <body>
     <div class="container">
     <div class="alert alert-warning" role='alert'>
-    <?php echo $erro; header("Refresh:1; url=/Agendamento/index");?>
+    <?php echo $erro;?>
     <div class='d-flex justify-content-center'>
 <div class='spinner-border' role='status'>
 </div>
