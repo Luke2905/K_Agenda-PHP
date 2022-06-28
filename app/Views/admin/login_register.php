edbd200197bec3dbf53af8d72538f3ac4948ebe6
KaioSena <88010959+KaioSena@users.noreply.github.com>
2022-06-27 21:53:09 -0300

    Add files via upload

---
+++ app/Views/admin/login_register.php
@@ -0,0 +1,39 @@
+<?php $this->extend("layout_padrao");?>
+
+<?php $this->section("login_register");?>
+
+<div class="container_login">
+    <div class="login_img">
+        <img alt="">
+    </div>
+    <div class="login_form">
+        <form action="/Login/autenticar" method="post">
+            <div class="login_header">
+                <div class="titulo_login">
+                    <h1>Login</h1>
+                </div>
+                <div class="login_button">
+                    <!--  <button><a href="/Home/register">Registrar</a></button> -->
+                </div>
+            </div>
+
+            <div class="input_grupo">
+                <div class="input_box">
+                    <label for="">Digite seu email</label>
+                    <input type="email" name="usuario" required>
+                </div>
+                <div class="input_box">
+                    <label for="">Digite sua senha</label>
+                    <input type="password" name="senha" required>
+                </div>
+            </div>
+
+            <div class="registrar">
+                <button  type="submit" >Entrar</button>
+            </div>
+
+        </form>
+    </div>
+</div>
+
+<?php $this->endSection(); ?>
